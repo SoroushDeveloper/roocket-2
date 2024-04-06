2831fa419c823d82549751da5e937a7a936dd7ef
SoroushDev <mr.soroosh.qr@gmail.com>
2024-04-06 22:00:21 +0330

    useFetch
    Took 14 minutes

--- src/components/blogs/allBlogs.jsx
+++ src/components/blogs/allBlogs.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import BlogsItem from './blogsItem'
-import BlogsItemLoading from './blogsItemLoading'
-import useBlogsData from '../../hooks/useBlogsData'
+import React from 'react';
+import BlogsItem from './blogsItem';
+import BlogsItemLoading from './blogsItemLoading';
+import useFetch from '../../hooks/useFetch.js';
 import Alert from '@mui/material/Alert';
-
-
 
 export default function AllBlogs() {
 
-  const url = 'https://65f7f726b4f842e808867f20.mockapi.io/rocket-1/api/blogs'
-  const {blogs,error,isLoading} = useBlogsData(url)
+    const url = 'https://65f7f726b4f842e808867f20.mockapi.io/rocket-1/api/blogs';
+    const {data: blogs, error, isLoading} = useFetch(url);
 
-  return (
-    <div className=''>
-        <h2 className=' text-left font-extrabold text-3xl mb-10'>Blogs</h2>
-        <div className='grid grid-cols-4 gap-x-12 gap-y-20'>
-            {blogs && !isLoading?(blogs.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((item,index)=>(
-                <BlogsItem blog={item} index={index} key={index}/>
-            )))
-            :<>
-            {[...Array(4)].map((_, index) => (
-                <div key={index} className=''>
-                    <BlogsItemLoading />
-                </div>
-            ))}
-            </>}
+    return (
+        <div className=''>
+            <h2 className=' text-left font-extrabold text-3xl mb-10'>Blogs</h2>
+            <div className='grid grid-cols-4 gap-x-12 gap-y-20'>
+                {blogs && !isLoading ? (blogs.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((item, index) => (
+                        <BlogsItem blog={item} index={index} key={index}/>
+                    )))
+                    : <>
+                        {[...Array(4)].map((_, index) => (
+                            <div key={index} className=''>
+                                <BlogsItemLoading/>
+                            </div>
+                        ))}
+                    </>}
+            </div>
+            {error ? <Alert severity="error">{error}</Alert> : ''}
         </div>
-        {error?<Alert severity="error">{error}</Alert>:''}
-    </div>
-  )
+    );
 }
--- src/components/blogs/latestBlogs.jsx
+++ src/components/blogs/latestBlogs.jsx
@@ -1,60 +1,55 @@
-import React from 'react'
-import { Navigation, Virtual } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import BlogsItem from './blogsItem'
-
-
+import React from 'react';
+import {Navigation, Virtual} from 'swiper/modules';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import BlogsItem from './blogsItem';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import BlogsItemLoading from './blogsItemLoading';
 import ShowMore from './showMore';
-import useBlogsData from '../../hooks/useBlogsData';
+import useFetch from '../../hooks/useFetch.js';
 import Alert from '@mui/material/Alert';
-
 
 export default function LatestBlogs() {
 
-  const url = 'https://65f7f726b4f842e808867f20.mockapi.io/rocket-1/api/blogs'
-  const {blogs,error,isLoading} = useBlogsData(url)
+    const url = 'https://65f7f726b4f842e808867f20.mockapi.io/rocket-1/api/blogs';
+    const {data: blogs, error, isLoading} = useFetch(url);
 
-
-  return (
-    <div className=''>
-        <div className='flex justify-between items-center'>
-            <h2 className=' text-left font-extrabold text-3xl mb-10'>LatestBlogs</h2>
-            <a href={`/blogs`}className='text-blue-500 cursor-pointer'>See All</a>
+    return (
+        <div className=''>
+            <div className='flex justify-between items-center'>
+                <h2 className=' text-left font-extrabold text-3xl mb-10'>LatestBlogs</h2>
+                <a href={`/blogs`} className='text-blue-500 cursor-pointer'>See All</a>
+            </div>
+            <div className='relative'>
+                <Swiper
+                    style={{
+                        '--swiper-navigation-sides-offset': '-50px'
+                    }}
+                    modules={[Navigation]} lazy={'true'} spaceBetween={50} navigation slidesPerView={4}>
+                    {!isLoading ? <>
+                            {blogs.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0, 10).map((item, index) => (
+                                <SwiperSlide key={index} className=''>
+                                    <BlogsItem blog={item} index={index} key={index}/>
+                                </SwiperSlide>
+                            ))}
+                            <SwiperSlide className=''>
+                                <ShowMore/>
+                            </SwiperSlide>
+                        </>
+                        :
+                        <>
+                            {[...Array(4)].map((_, index) => (
+                                <SwiperSlide key={index} className=''>
+                                    <BlogsItemLoading/>
+                                </SwiperSlide>
+                            ))}
+                        </>
+                    }
+                </Swiper>
+            </div>
+            {error ? <Alert severity="error">{error}</Alert> : ''}
         </div>
-        <div className='relative'>
-        <Swiper
-        style={{
-            '--swiper-navigation-sides-offset':'-50px'
-        }} 
-        modules={[Navigation]} lazy={'true'} spaceBetween={50} navigation slidesPerView={4}>
-            
-            {!isLoading?<>
-            {blogs.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0,10).map((item,index)=>(
-                <SwiperSlide key={index} className=''>
-                     <BlogsItem blog={item} index={index} key={index} />
-                </SwiperSlide>
-            ))}
-            <SwiperSlide className='' >
-                <ShowMore />
-            </SwiperSlide>
-            </>
-            :
-            <>
-            {[...Array(4)].map((_, index) => (
-                <SwiperSlide key={index} className=''>
-                    <BlogsItemLoading />
-                </SwiperSlide>
-            ))}
-            </>
-            }
-        </Swiper>
-        </div>
-        {error?<Alert severity="error">{error}</Alert>:''}
-    </div>
-  )
+    );
 }
